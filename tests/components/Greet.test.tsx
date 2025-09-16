643c255cf9d3a2691fbd735f7521973373f2216a
FachryIkhsal890 <fachryikhsal890@gmail.com>
2025-09-16 13:30:39 +0700

    Add greet and User account component test-2

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,18 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet-Component", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Mosh" />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should render login button when the name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button", { name: /login/i });
+    expect(button).toBeInTheDocument();
+  });
+});
